5b95ec9660bdd98cc553f6f310679c396ce81afe
xiaoming <654757329@qq.com>
2019-03-28 16:22:20 +0800

    init

---
+++ LIAN/js/index.js
@@ -0,0 +1,18 @@
+/*
+* 入口文件
+* */
+
+import {add,sub} from './module1';
+import {mul} from './module2';
+/*import data from '../json/data.json';
+import '../less/demo.less';*/
+
+console.log(add(3,4));
+console.log(sub(4,3));
+console.log(mul(4,3));
+/*
+console.log(data,typeof data);
+
+setTimeout(()=>{
+
+},200);*/
